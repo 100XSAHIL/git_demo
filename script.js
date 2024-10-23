98dea239551a65d08ce0ee215a5bcb58e9298710
100XSAHIL <sahilmishra0275@gmail.com>
2024-10-24 00:09:45 +0530

    index and script files changes

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ for (let i = 0; i < 10; i++) {
     randomArray.push(getRandomNumber(1, 100));
     randomArray.push(getRandomNumber(1, 100));
 }
-
+console.log("MY name is sahil mishra")
 
 console.log("chnage done by me ")
 console.log(randomArray);
